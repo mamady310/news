1a3e3e5fc8eb7eb757a748929ae0f25854439fdc
Mamady Sillah <supurpisces310@msn.com>
2020-05-10 17:24:41 -0400

    added links to navbar.js and then put in properties to allow a user to scroll to a component on click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,36 @@
-import React, { Component } from react;
+import React, { Component } from 'react';
+import { Link } from 'react-scroll';
 
 class NavBar extends Component {
     render () {
         return (
-            <div></div>
+            <div className="navbar">
+              <Link
+    activeClass="active"
+    to="main"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration= {500}
+>Main</Link>
+<Link
+    activeClass="active"
+    to="entertainment"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration= {500}
+>Entertainment</Link>
+<Link
+    activeClass="active"
+    to="technology"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration= {500}
+>Technology</Link>
+
+            </div>
         )
     }
 } 
