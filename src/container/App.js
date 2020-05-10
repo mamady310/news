5340e2267428b221b76b9f5749461041ebf7cd52
Mamady Sillah <supurpisces310@msn.com>
2020-05-10 17:08:22 -0400

    created main, technology, and entertainmet components

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from '../components/NavBar';
+import Main from '../components/Main';
+import Entertainment from '../components/Entertainment';
+import Technology from '../components/Technology';
 
 class App extends Component {
   render () {
